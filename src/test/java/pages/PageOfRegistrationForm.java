53eb2e6065dcc941957da3ebf897e0a0b4823a8b
Loarlam <Loarlam@ya.ru>
2022-06-08 00:43:03 +0300

    Созданы классы календаря и формы результирующих данных.

---
+++ src/test/java/pages/PageOfRegistrationForm.java
@@ -0,0 +1,13 @@
+package pages;
+
+import pages.pagecomponents.CalenderComponent;
+import pages.pagecomponents.ResultDataFormComponent;
+
+public class PageOfRegistrationForm {
+    CalenderComponent calenderComponent = new CalenderComponent();
+    ResultDataFormComponent resultDataFormComponent = new ResultDataFormComponent();
+
+    public void PageOfRegistrationForm() {
+
+    }
+}
